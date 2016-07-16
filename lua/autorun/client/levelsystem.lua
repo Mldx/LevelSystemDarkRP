607ab25b775f89458baa2042de1d8ad352a6059f
Mathias Christensen <mldx@mldx.dk>
2016-07-17 00:08:34 +0200

    Initial Commit
    This is the first commit, the level system works in this version, but
    you can't use level for anything.

---
+++ lua/autorun/client/levelsystem.lua
@@ -0,0 +1,55 @@
+--CLIENT SCRIPT
+
+xp = 0
+returnxp = 0
+xpneeded = 200
+level = 1
+
+function Round(num, idp)
+	local mult = 10^(idp or 0)
+	local out = math.floor(num * mult + 0.5) / mult
+	if out - math.floor(out) == 0 then
+		for i = 1, idp - 1 do
+			out = out .. ".0"
+		end
+	end
+	return out
+end
+
+net.Receive( "xp", function()
+	level = net.ReadInt(32)
+	xp = net.ReadInt(32)
+	xpneeded = net.ReadInt(32)
+	if xp%100
+	print("level: " .. level .. "   xp: " .. xp .. "   xpneeded: " .. xpneeded)
+	if Round((xp / xpneeded)*200,1) < 8 then
+		returnxp = 8
+	else
+		returnxp = (xp/xpneeded)*200
+	end
+
+end )
+
+timer.Create("xp", 1, 0, function()
+	xp = xp+1
+	if xp >= xpneeded then
+		level = level + 1
+		xp = 0
+		xpneeded = xpneeded + 100
+	end
+	if (xp / xpneeded)*200 < 8 then
+		returnxp = 8
+	else
+		returnxp = Round((xp/xpneeded)*200,1)
+	end
+end)
+
+function Draw()
+	draw.RoundedBox(4, ScrW() / 2 - 105, ScrH()-45, 210, 35, Color(0,0,0,100)) -- BACKGROUND BLACK
+	draw.RoundedBox(4, ScrW() / 2 - 102, ScrH()-42, 204, 29, Color(0,190,0,100)) -- BACKGROUND GREEN
+	draw.RoundedBox(4, ScrW() / 2 - 100, ScrH()-40, returnxp, 25, Color(50,255,50,100)) -- PROGRESS BAR 200 = 100%
+	draw.DrawText("Level: " .. level, "BebasNeue", ScrW() / 2, ScrH()-44, Color(0, 0, 0, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER) -- X, Y, Width, Height, Color 
+	draw.DrawText(Round((xp/xpneeded)*100, 2) .. "%", "BebasNeue", ScrW() / 2, ScrH()-29, Color(0, 0, 0, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER) -- X, Y, Width, Height, Color
+end
+
+hook.Add("HUDPaint", "Draw", Draw)
---
+++ lua/autorun/server/levelsystem.lua
@@ -0,0 +1,101 @@
+--SERVER SCRIPT
+util.AddNetworkString("xp")	
+
+local function tables_exist()
+	--sql.Query("DROP TABLE players_levelsystem;")
+	if sql.TableExists("players_levelsystem") then
+		MsgC( Color( 0, 255, 0 ), "Level System: ")
+		print("Using existing database.")
+
+	else
+		query = "CREATE TABLE players_levelsystem (steamid varchar(255),level int,xp int,xpneeded int, UNIQUE(steamid))";
+		result = sql.Query(query)
+		if (sql.TableExists("players_levelsystem")) then
+			MsgC( Color( 0, 255, 0 ), "Level System: ")
+			print("The database was successfully created.")
+		else
+			MsgC( Color( 0, 255, 0 ), "Level System: ")
+			print("Failed to create the database.")
+			print(sql.LastError(result))
+		end
+	end
+end
+
+local function sql_getvalues (pl)
+	pl:SetNWInt("level", sql.QueryValue("SELECT level FROM players_levelsystem WHERE steamid = '"..steamID.."'"))
+	pl:SetNWInt("xp", sql.QueryValue("SELECT xp FROM players_levelsystem WHERE steamid = '"..steamID.."'"))
+	pl:SetNWInt("xpneeded", sql.QueryValue("SELECT xpneeded FROM players_levelsystem WHERE steamid = '"..steamID.."'"))
+end
+
+local function new_player(pl)
+	steamID = pl:SteamID()
+	sql.Query("INSERT OR IGNORE INTO players_levelsystem (`steamid`, `level`, `xp`, `xpneeded`) VALUES ('"..steamID.."', '1', '0', '200')")
+	result = sql.Query("SELECT steamid, level, xp, xpneeded FROM players_levelsystem WHERE steamid = '"..steamID.."'")
+	if (result) then
+		sql_getvalues(pl)
+	else
+		MsgC( Color( 0, 255, 0 ), "Level System: ")
+		print("Something went wrong creating the new player.")
+		print(sql.LastError(result))
+	end
+end
+
+--local function player_exists(pl)
+--	steamID = pl:GetNWString("SteamID")
+--	result = sql.Query("SELECT steamid, level, xp, xpneeded FROM players_levelsystem WHERE steamid = '"..steamID.."'")
+--	if (result) then
+--		sql_levelsystem(pl)
+--		print("Level System: An already existing user joined the server.")
+--	else
+--		new_player(steamID, pl)
+--	end
+--end
+
+local function savelevel(pl)
+	unique_id = pl:GetNWString("SteamID")
+	level = pl:GetNWInt("level")
+	xp = pl:GetNWInt("xp")
+	xpneeded = pl:GetNWInt("xpneeded")
+	sql.Query("UPDATE players_levelsystem SET level = "..level..", xp = "..xp..", xpneeded = "..xpneeded.." WHERE steamid = '"..unique_id.."'")
+end
+
+hook.Add("PlayerInitialSpawn", "MyXP_FirstSpawn", function(pl) 
+	pl:SetNWInt("xp", 0)
+	pl:SetNWInt("level", 1)
+	pl:SetNWInt("xpneeded", 200)
+end)
+
+local function InitialSpawnDB(pl)
+	timer.Create("Steam_id_delay", 1, 1, function() --Sets up a little delay cause otherwise the steamid wont be available yet
+		new_player(pl) --Calls our player exists local function
+	end)
+end
+
+hook.Add( "PlayerInitialSpawn", "InitialSpawnDB", InitialSpawnDB )	
+
+
+timer.Create("XPTimer", 10, 0, function()  --Sync Database with the server, and Sync Client with server.
+
+	for k, v in pairs(player.GetAll()) do
+	
+		v:SetNWInt("xp", v:GetNWInt("xp") + 10)
+		if tonumber(v:GetNWInt("xp")) >= tonumber(v:GetNWInt("xpneeded")) then 
+			v:SetNWInt("xp", 0)
+			v:SetNWInt("level", v:GetNWInt("level")+1)
+			v:SetNWInt("xpneeded", v:GetNWInt("xpneeded")+100)
+			PrintMessage( HUD_PRINTTALK, v:GetName() .. " just leveled up to level " .. v:GetNWInt("level") .. "!" )
+		end
+		
+		savelevel(v)
+
+		net.Start("xp")
+		net.WriteInt(v:GetNWInt("level"), 32)
+		net.WriteInt(v:GetNWInt("xp"), 32)
+		net.WriteInt(v:GetNWInt("xpneeded"), 32)
+		net.Send(v)
+		
+	end
+	
+end)
+
+tables_exist()	
